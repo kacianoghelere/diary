9a652fcda07e14c1c654e622817681c2144aabff
Kaciano Ghelere <themohawkeagle@gmail.com>
2018-05-07 17:05:10 -0300

    Testando validação

--- src/app/App.js
+++ src/app/App.js
@@ -5,9 +5,7 @@ import { connect } from 'react-redux';
 import NoteForm from './components/NoteForm';
 import NoteList from './components/NoteList';
 
-class App extends Component{
-  const teste
-
+class App extends Component {
   render() {
     return (
       <div className="container pt-3 pb-3">
