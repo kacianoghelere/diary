4c61389ae78b4aa4f5f3aab6f97782979194f078
Kaciano Ghelere <themohawkeagle@gmail.com>
2018-05-08 09:03:05 -0300

    Teste

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyD-8m_M9RcGQXd4Swf8-fL8a3TolzvM6SY",
-  authDomain: "diary-441a5.firebaseapp.com",
-  databaseURL: "https://diary-441a5.firebaseio.com",
-  projectId: "diary-441a5",
-  storageBucket: "",
-  messagingSenderId: "218322295204"
+  apiKey: 'AIzaSyD-8m_M9RcGQXd4Swf8-fL8a3TolzvM6SY',
+  authDomain: 'diary-441a5.firebaseapp.com',
+  databaseURL: 'https://diary-441a5.firebaseio.com',
+  projectId: 'diary-441a5',
+  storageBucket: '',
+  messagingSenderId: '218322295204'
 };
 
 firebase.initializeApp(config);
